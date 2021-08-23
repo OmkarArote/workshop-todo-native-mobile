aec507eb36cc492aac4a72e6994a374601e352d0
David Jones-Gilardi <david.gilardi@datastax.com>
2021-08-23 14:31:27 -0400

    Update hostURL.sh

--- hostURL.sh
+++ hostURL.sh
@@ -4,7 +4,7 @@ PORT="8888"
 IS_PROD="false"
 GITPOD="true"
 
-echo "\nHOST=$URL" >> .env
+echo -e "\nHOST=$URL" >> .env
 echo "PORT=$PORT" >> .env
 echo "IS_PROD=$IS_PROD" >> .env
 echo "GITPOD=$GITPOD" >> .env
