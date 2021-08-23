df2cb7152babcf456068991df0692e3f94c4056f
RyanWelford <ryan.welford@datastax.com>
2021-08-23 12:59:19 -0500

    hostURL script

---
+++ hostURL.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+URL=`gp url 8888`
+PORT="8888"
+IS_PROD="false"
+GITPOD="true"
+
+echo "\nHOST=$URL" >> .env
+echo "PORT=$PORT" >> .env
+echo "IS_PROD=$IS_PROD" >> .env
+echo "GITPOD=$GITPOD" >> .env
