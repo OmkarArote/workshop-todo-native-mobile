f0ae7cb0c105ca8bb3240869c66def730fdcda75
soniaisiganporia <82838476+soniaisiganporia@users.noreply.github.com>
2021-08-23 22:20:48 -0700

    Clean up babel.config file

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      //"transform-inline-environment-variables"
       "inline-dotenv"
     ]
-      /*"module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-    }]]*/
   };
 };
